e66e6eb0b20df7e31ce22e5f1ea5f03132a4d431
gmollison <gmollison@google.com>
2016-05-20 08:02:04 +0100

    minor amendment to setup.sh

--- resources/setup.sh
+++ resources/setup.sh
@@ -12,5 +12,5 @@ mkdir /tmp/creds
 bq mk sandiego_freeways
 bq mk --schema geocoded_journeys.json sandiego_freeways.geocoded_journeys
 mkdir /tmp/creds/data
-cp resources/data/* /tmp/creds/data/
-cp resources/setup.yaml /tmp/creds/
+cp data/* /tmp/creds/data/
+cp setup.yaml /tmp/creds/
